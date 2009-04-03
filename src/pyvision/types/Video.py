47f5ebc3d18241273107d30b10b32533d47878c2
bolme <bolme@2a83181e-c148-0410-bf7c-9f1693d9d8f0>
2009-04-03 01:34:09 +0000

    Fixed a memory leak with the OpenCV video input and added support for ffmpeg command line input.
    git-svn-id: https://pyvision.svn.sourceforge.net/svnroot/pyvision/trunk@71 2a83181e-c148-0410-bf7c-9f1693d9d8f0

--- src/pyvision/types/Video.py
+++ src/pyvision/types/Video.py
@@ -34,15 +34,24 @@
 import opencv
 import opencv.highgui
 import time
+import subprocess
+import os
 #import highgui
 
 from pyvision.types.Image import Image
 from pyvision.edge.canny import canny
 from pyvision.analysis.ImageLog import ImageLog
 
+import pyvision as pv
+import opencv as cv
+
+from scipy import weave
+
 class Webcam:
     def __init__(self,camera_num=1,size=(640,480)):
-        self.cv_capture = opencv.highgui.cvCreateCameraCapture( camera_num )
+
+        self.cv_capture = opencv.highgui.cvCreateCameraCapture( camera_num )        
+        
         self.size = size
     
     def query(self):
@@ -92,6 +101,26 @@ class Video:
         self.n_frames = opencv.highgui.cvGetCaptureProperty(self.cv_capture,opencv.highgui.CV_CAP_PROP_FRAME_COUNT)
         self.current_frame = 0
         
+    def __del__(self):
+        #opencv.highgui.cvReleaseCapture(self.cv_capture)
+        
+
+        # cvReleaseCapture interface does not work so use weave this may be fixed in release 1570
+        # TODO: This should be removed when the opencv bug is fixed
+        capture = self.cv_capture.__int__()
+        weave.inline(
+            '''
+            CvCapture* tmp = (CvCapture*) capture;
+            cvReleaseCapture(&tmp);
+            ''',
+            arg_names=['capture'],
+            type_converters=weave.converters.blitz,
+            include_dirs=['/usr/local/include'],
+            headers=['<opencv/cv.h>','<opencv/highgui.h>'],
+            library_dirs=['/usr/local/lib'],
+            libraries=['cv','highgui']
+        )
+
     def query(self):
         if self.current_frame >= self.n_frames:
             return None
@@ -128,4 +157,90 @@ class Video:
         return frame
         
                 
+        
+class FfmpegIn:
+    # TODO: there may be a bug with the popen interface
+    
+    def __init__(self,filename,size=None,aspect=None,options=""):
+        self.filename = filename
+        self.size = size
+        self.aspect = aspect
+        
+        # Open a pipe
+        args = "/opt/local/bin/ffmpeg -i %s %s -f yuv4mpegpipe - "%(filename,options)
+        #print args
+        
+        self.stdin, self.stdout, self.stderr = os.popen3(args)
+        #popen = subprocess.Popen(args,executable="/opt/local/bin/ffmpeg")
+        
+        line = self.stdout.readline()
+        print line
+        #self.stdout.seek(0,os.SEEK_CUR)
+        
+        format,w,h,f,t1,aspect,t2,t3 = line.split()
+        
+        # I am not sure what all this means but I am checking it anyway
+        assert format=='YUV4MPEG2'
+        #assert t1=='Ip'
+        assert t2=='C420mpeg2'
+        assert t3=='XYSCSS=420MPEG2'
+
+        # get the width and height
+        assert w[0] == "W"
+        assert h[0] == "H"
+        
+        self.w = int(w[1:])
+        self.h = int(h[1:])
+        
+        # Create frame caches        
+        if size == None and self.aspect != None:
+            h = self.h
+            w = int(round(self.aspect*h))
+            size = (w,h)
+            #print size
+        
+        self.size = size
+        
+        self.frame_y = cv.cvCreateImage( cv.cvSize(self.w,self.h), cv.IPL_DEPTH_8U, 1 )
+        self.frame_u2 = cv.cvCreateImage( cv.cvSize(self.w/2,self.h/2), cv.IPL_DEPTH_8U, 1 )
+        self.frame_v2 = cv.cvCreateImage( cv.cvSize(self.w/2,self.h/2), cv.IPL_DEPTH_8U, 1 )
+
+        self.frame_u = cv.cvCreateImage( cv.cvSize(self.w,self.h), cv.IPL_DEPTH_8U, 1 )
+        self.frame_v = cv.cvCreateImage( cv.cvSize(self.w,self.h), cv.IPL_DEPTH_8U, 1 )
+        self.frame_col = cv.cvCreateImage( cv.cvSize(self.w,self.h), cv.IPL_DEPTH_8U, 3 )
+
+        
+        if self.size != None:
+            w,h = self.size
+            self.frame_resized = cv.cvCreateImage(cv.cvSize(w,h),cv.IPL_DEPTH_8U,3)
+
+        
+        
+    def frame(self):
+        line = self.stdout.readline()
+        #print line
+        #print self.w,self.h
+        y = self.stdout.read(self.w*self.h)
+        u = self.stdout.read(self.w*self.h/4)
+        v = self.stdout.read(self.w*self.h/4)
+        if len(y) < self.w*self.h:
+            raise EOF
+        
+        self.frame_y.imageData=y
+        self.frame_u2.imageData=u
+        self.frame_v2.imageData=v
+
+        cv.cvResize(self.frame_u2,self.frame_u)
+        cv.cvResize(self.frame_v2,self.frame_v)
+        
+        cv.cvMerge(self.frame_y,self.frame_u,self.frame_v,None,self.frame_col)
+        cv.cvCvtColor(self.frame_col,self.frame_col,cv.CV_YCrCb2RGB)
+        
+        out = self.frame_col
+        
+        if self.size != None:
+            cv.cvResize(self.frame_col,self.frame_resized)
+            out = self.frame_resized
+
+        return pv.Image(self.frame_y),pv.Image(self.frame_u),pv.Image(self.frame_v),pv.Image(out)
         
