821f213bcd97b6b115900da402ec6c25c1905d43
svohara <svohara@2a83181e-c148-0410-bf7c-9f1693d9d8f0>
2010-09-19 04:06:13 +0000

    Changed to remove "weave" code and use openCV's python interface for ExtractSURF. More work is needed to return the descriptors list and a few other things -- see the TODO's in the code comments.
    git-svn-id: https://pyvision.svn.sourceforge.net/svnroot/pyvision/trunk@163 2a83181e-c148-0410-bf7c-9f1693d9d8f0

--- src/pyvision/point/DetectorSURF.py
+++ src/pyvision/point/DetectorSURF.py
@@ -4,8 +4,9 @@ Created on Apr 16, 2009
 @author: bolme
 '''
 from pyvision.point.DetectorROI import DetectorROI
-import pyvision as pv
-from scipy import weave
+#import pyvision as pv
+#from scipy import weave
+import cv
 
 def ExtractSURF(im,min_hessian=300):
     '''
@@ -16,48 +17,60 @@ def ExtractSURF(im,min_hessian=300):
     TODO: I believe there are no memory leaks but this should be checked. cvSURFParams?
     '''
     cvim= im.asOpenCVBW()
-    mat = int(cvim.this)
+    #mat = int(cvim.this)
     min_hessian = float(min_hessian)
+    #TODO: OpenCV python interface now includes cv.ExtractSURF(cvim, mask, storage, params)
+    #This is my (Steve's) attempt at this, but I am concerned we're not returning the
+    # some of the information once this gets back to the caller...perhaps the parent
+    # class is filtering out the addtnl data that SURF points provide?
     
-    keypoints = weave.inline(
-        '''   
-        CvMat* im = (CvMat*)mat;
-        
-        CvMemStorage* storage = cvCreateMemStorage();
-        CvSeq* keypoints = cvCreateSeq(0,sizeof(CvSeq),sizeof(CvSURFPoint),storage);
-        cvExtractSURF(im,NULL,&keypoints,NULL,storage,cvSURFParams(min_hessian));
-        
-        
-        int n = keypoints->total;
-        PyObject* list = PyList_New(n);
-        CvSURFPoint pt;
-        for(int i = 0 ; i < n; i++){
-            cvSeqPop(keypoints,&pt);
-            
-            PyObject* tuple = PyTuple_New(5);
-            PyTuple_SetItem(tuple, 1, PyFloat_FromDouble(pt.pt.x));
-            PyTuple_SetItem(tuple, 2, PyFloat_FromDouble(pt.pt.y));
-            PyTuple_SetItem(tuple, 3, PyInt_FromLong(pt.size));
-            PyTuple_SetItem(tuple, 4, PyFloat_FromDouble(pt.dir));
-            PyTuple_SetItem(tuple, 0, PyFloat_FromDouble(pt.hessian));
-            
-            PyList_SetItem(list,i,tuple);
-            //printf("%5d %10.5f %10.5f %5d %10.5f %10.5f\\n", i, pt.pt.x, pt.pt.y, pt.size, pt.dir, pt.hessian);
-        
-        
-        cvClearMemStorage(storage);
-        cvReleaseMemStorage(&storage);
-        
-        return_val = list;
-        ''',
-        arg_names=['mat','min_hessian'],
-        include_dirs=['/usr/local/include'],
-        headers=['<opencv/cv.h>'],
-        library_dirs=['/usr/local/lib'],
-        libraries=['cv']
-    )
+    #TODO: Now that we have the descriptors, we need to return them to user if desired.
+    (keypts, descriptors) = cv.ExtractSURF(cvim, None, cv.CreateMemStorage(), (0, min_hessian, 3, 1))
+    
+    keypoints = list()
+    for ((x, y), laplacian, size, dir, hessian) in keypts:
+        keypoints.append((hessian,x,y,size,dir,laplacian) )
     
     return keypoints
+#   keypoints = weave.inline(
+#        '''   
+#        CvMat* im = (CvMat*)mat;
+#        
+#        CvMemStorage* storage = cvCreateMemStorage();
+#        CvSeq* keypoints = cvCreateSeq(0,sizeof(CvSeq),sizeof(CvSURFPoint),storage);
+#        cvExtractSURF(im,NULL,&keypoints,NULL,storage,cvSURFParams(min_hessian));
+#        
+#        
+#        int n = keypoints->total;
+#        PyObject* list = PyList_New(n);
+#        CvSURFPoint pt;
+#        for(int i = 0 ; i < n; i++){
+#            cvSeqPop(keypoints,&pt);
+#            
+#            PyObject* tuple = PyTuple_New(5);
+#            PyTuple_SetItem(tuple, 1, PyFloat_FromDouble(pt.pt.x));
+#            PyTuple_SetItem(tuple, 2, PyFloat_FromDouble(pt.pt.y));
+#            PyTuple_SetItem(tuple, 3, PyInt_FromLong(pt.size));
+#            PyTuple_SetItem(tuple, 4, PyFloat_FromDouble(pt.dir));
+#            PyTuple_SetItem(tuple, 0, PyFloat_FromDouble(pt.hessian));
+#            
+#            PyList_SetItem(list,i,tuple);
+#            //printf("%5d %10.5f %10.5f %5d %10.5f %10.5f\\n", i, pt.pt.x, pt.pt.y, pt.size, pt.dir, pt.hessian);
+#        
+#        
+#        cvClearMemStorage(storage);
+#        cvReleaseMemStorage(&storage);
+#        
+#        return_val = list;
+#        ''',
+#        arg_names=['mat','min_hessian'],
+#        include_dirs=['/usr/local/include'],
+#        headers=['<opencv/cv.h>'],
+#        library_dirs=['/usr/local/lib'],
+#        libraries=['cv']
+#    )
+    
+    #return keypoints
 
 
 class DetectorSURF(DetectorROI):
@@ -70,5 +83,5 @@ class DetectorSURF(DetectorROI):
     
     def _detect(self,im):
         keypoints = ExtractSURF(im,min_hessian=self.min_hessian)
-        keypoints.sort(lambda x,y: -cmp(x[0],y[0]))
+        keypoints.sort(lambda x,y: -cmp(x[0],y[0]))         
         return keypoints
