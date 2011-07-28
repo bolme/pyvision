05f61918b958da0cc4281fa95fa51a138823b72f
bolme <bolme@2a83181e-c148-0410-bf7c-9f1693d9d8f0>
2011-07-28 05:20:23 +0000

    Improved documentation.
    git-svn-id: https://pyvision.svn.sourceforge.net/svnroot/pyvision/trunk@334 2a83181e-c148-0410-bf7c-9f1693d9d8f0

--- src/pyvision/other/surf.py
+++ src/pyvision/other/surf.py
@@ -9,6 +9,15 @@ import cv
 
 def surf(im,mask=None,extended=False,hessianThreshold=500, nOctaves=3, nOctaveLayers=4):
     '''
+    Keypoints contain a
+      0: center point
+      1 sign of laplacian (-1,0,+1)
+      2 scale - diameter or radius
+      3 angle
+      4 response value
+    
+    Descriptors contain 64 floating point numbers
+    
     @param im: image to extract features from.
     @type im:  pv.Image
     @param mask: a mask that controls where features are extracted from.
