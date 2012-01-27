cac71c71561e71fab748fe274e4ee5c896b0fb7b
svohara <svohara@2a83181e-c148-0410-bf7c-9f1693d9d8f0>
2012-01-27 17:36:41 +0000

    git-svn-id: https://pyvision.svn.sourceforge.net/svnroot/pyvision/trunk@419 2a83181e-c148-0410-bf7c-9f1693d9d8f0

--- src/samples/WACV2012_Tutorial/tutorials/TutorialFaceDetect.py
+++ src/samples/WACV2012_Tutorial/tutorials/TutorialFaceDetect.py
@@ -6,9 +6,8 @@ if __name__ == '__main__':
     detector = cd.CascadeDetector()
     
     cam = pv.Webcam()
-    while True:
-        frame = cam.query()
+    for frame in cam:
         rects = detector(frame)
         for rect in rects:
             frame.annotateRect(rect)
-        frame.show()
+        frame.show(delay=30)
