d83b400cbbe4dbf823e2d4e869d71c888b02de5e
David Bolme <bolmeds@ornl.gov>
2013-06-07 15:11:02 -0400

    Checking in superpixel stuff

--- src/pyvision/segment/superpixel.py
+++ src/pyvision/segment/superpixel.py
@@ -11,14 +11,15 @@ import pyvision as pv
 import numpy as np
 
 
-def slic(im,k,blur=7,color_weight=0.30):
+def slic(im,k,blur=None,color_weight=0.30):
     '''
     This is a k-means based super pixel algorithm inspired by slic.
     
     http://ivrg.epfl.ch/supplementary_material/RK_SLICSuperpixels/index.html
     '''
     cvmat = im.asOpenCV2()
-    cvmat = cv2.blur(cvmat, (blur,blur))
+    if blur != None:
+        cvmat = cv2.blur(cvmat, (blur,blur))
     mat = color_weight*cvmat
     r,c,_ = mat.shape
     w,h = im.size
@@ -35,9 +36,10 @@ def slic(im,k,blur=7,color_weight=0.30):
     return labels,mask
 
 if __name__ == '__main__':
-    im = pv.Image(pv.FRUITS )
+    im = pv.Image(pv.FRUITS)
     #im = im.resize((100,100))
-    labels = slic(im,300)
+    labels,mask = slic(im,300)
     pv.AffineTranslate(1,0,im.size)
     pv.Image(np.float32(labels)).show()
+    im.annotateMask(mask)
     im.show()
